a83192f5c482c65ebadc066d4d6e15504e73399a
mouni276 <121158250+mouni276@users.noreply.github.com>
2023-01-06 17:01:33 +0530

    Add files via upload

---
+++ day15program_01.py
@@ -0,0 +1,8 @@
+class Vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+
+
+mode1x=Vehicle(240,18)
+print(mode1x.max_speed,mode1x.mileage)
